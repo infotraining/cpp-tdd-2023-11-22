610af3cb56f4d360da501c109841967106a1136a
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-22 16:08:33 +0100

    GTest test grouping

--- tdd-katas/gtest/bowling/src/bowling.hpp
+++ tdd-katas/gtest/bowling/src/bowling.hpp
@@ -4,11 +4,77 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <array>
 
-// namespace TDD
-// {
-//     class BowlingGame   
-//     {};
-// }
+namespace TDD
+{
+class BowlingGame
+{
+public:
+    int score() const
+    {
+        int result{};
+        int roll_index{};
+        for(int i = 0; i < frames_count; ++i)
+        {
+            auto [current_frame_score, frame_size] = frame_score(roll_index);
+            result += current_frame_score;
+            
+            if (is_strike(roll_index))
+            {
+                result += strike_bonus(roll_index);
+            }
+            else if (is_spare(roll_index))
+            {
+                 result += spare_bonus(roll_index);                
+            }
+            roll_index += frame_size;
+        }
+        
+        return result;
+    }
+
+    void roll(int pins)
+    {
+        rolls_.at(current_roll++) = pins;
+    }
+private:
+    static constexpr int max_pins_in_frame = 10;
+    static constexpr int frames_count = 10;
+
+    int current_roll{0};
+    std::array<int, 21> rolls_ {};
+
+    bool is_spare(int roll_index) const
+    {
+        return rolls_.at(roll_index) + rolls_.at(roll_index+1) == max_pins_in_frame;
+    }
+
+    bool is_strike(int roll_index) const
+    {
+        return rolls_.at(roll_index) == max_pins_in_frame;
+    }
+
+    std::pair<int, int> frame_score(int roll_index) const
+    {
+        if (is_strike(roll_index))
+        {
+            return std::pair{max_pins_in_frame, 1};
+        }
+
+        return std::pair{rolls_.at(roll_index) + rolls_.at(roll_index+1), 2};
+    }
+
+    size_t spare_bonus(size_t roll_index) const 
+    {
+        return rolls_[roll_index + 2];
+    }
+
+    size_t strike_bonus(size_t roll_index) const 
+    {
+        return rolls_[roll_index + 1] + rolls_[roll_index + 2];
+    }
+};
+}
 
 #endif
--- tdd-katas/gtest/bowling/tests/bowling_tests.cpp
+++ tdd-katas/gtest/bowling/tests/bowling_tests.cpp
@@ -9,74 +9,7 @@
 #include "bowling.hpp"
 
 using namespace std;
-
-class BowlingGame
-{
-public:
-    int score() const
-    {
-        int result{};
-        int roll_index{};
-        for(int i = 0; i < frames_count; ++i)
-        {
-            auto [current_frame_score, frame_size] = frame_score(roll_index);
-            result += current_frame_score;
-            
-            if (is_strike(roll_index))
-            {
-                result += strike_bonus(roll_index);
-            }
-            else if (is_spare(roll_index))
-            {
-                 result += spare_bonus(roll_index);                
-            }
-            roll_index += frame_size;
-        }
-        
-        return result;
-    }
-
-    void roll(int pins)
-    {
-        rolls_.at(current_roll++) = pins;
-    }
-private:
-    static constexpr int max_pins_in_frame = 10;
-    static constexpr int frames_count = 10;
-
-    int current_roll{0};
-    std::array<int, 21> rolls_ {};
-
-    bool is_spare(int roll_index) const
-    {
-        return rolls_.at(roll_index) + rolls_.at(roll_index+1) == max_pins_in_frame;
-    }
-
-    bool is_strike(int roll_index) const
-    {
-        return rolls_.at(roll_index) == max_pins_in_frame;
-    }
-
-    std::pair<int, int> frame_score(int roll_index) const
-    {
-        if (is_strike(roll_index))
-        {
-            return std::pair{max_pins_in_frame, 1};
-        }
-
-        return std::pair{rolls_.at(roll_index) + rolls_.at(roll_index+1), 2};
-    }
-
-    size_t spare_bonus(size_t roll_index) const 
-    {
-        return rolls_[roll_index + 2];
-    }
-
-    size_t strike_bonus(size_t roll_index) const 
-    {
-        return rolls_[roll_index + 1] + rolls_[roll_index + 2];
-    }
-};
+using namespace TDD;
 
 class BowlingGameTests : public ::testing::Test // Arrange
 {   
@@ -227,4 +160,56 @@ BowlingGameParams params[] = {
 };
 
 INSTANTIATE_TEST_SUITE_P(PackOfBowlingTests, BowlingGameParamTests, ::testing::ValuesIn(params));
+
+
+struct VectorDefaultInitialized : public ::testing::Test
+{
+    std::vector<int> vec;
+};
+
+TEST_F(VectorDefaultInitialized, WhenDefaultCreatedSizeIsZero)
+{
+    std::vector<int> vec;
+
+    ASSERT_EQ(vec.size(), 0);
+}
+
+TEST_F(VectorDefaultInitialized, WhenDefaultCreatedCapacityIsZero)
+{
+    std::vector<int> vec;
+
+    ASSERT_EQ(vec.capacity(), 0);
+}
+
+struct VectorWithItems : public ::testing::Test
+{
+    std::vector<int> vec = {1, 2, 3};
+};
+
+struct VectorAsDynamicArray : VectorWithItems
+{};
+
+struct VectorIndexing : VectorWithItems
+{};
+
+TEST_F(VectorAsDynamicArray, PushBackIncreasesSize)
+{
+    vec.push_back(1);
+    ASSERT_EQ(vec.size(), 4);
+}
+
+TEST_F(VectorAsDynamicArray, PushBackIncreasesCapacity)
+{
+    size_t prev_capacity = vec.capacity();
+    
+    vec.push_back(1);
+    
+    ASSERT_GT(vec.capacity(), prev_capacity);
+    ASSERT_GE(vec.capacity(), vec.size());
+}
+
+TEST_F(VectorIndexing, CanBeIndexedUsingBrackets)
+{
+    ASSERT_EQ(vec[1], 2);
+}
 
