9abde07d86139695250a386b8f9ea94f4b89d546
Piotr Szegda <Piotr.Szegda@motorolasolutions.com>
2023-11-23 16:25:28 +0100

    work

--- tdd-katas/catch2/mars-rover-catch/src/rover.hpp
+++ tdd-katas/catch2/mars-rover-catch/src/rover.hpp
@@ -11,8 +11,76 @@
 #include <vector>
 #include <memory>
 
+
 namespace TDD
 {
-}
+    class World;
+    enum Direction
+    {
+        N,
+        W,
+        S,
+        E,
+    };
+
+    struct Position
+    {
+        int x;
+        int y;
+        Direction dir;
+
+        bool operator==(const Position& rhs) const = default;
+
+        friend std::ostream& operator<<(std::ostream& out, const Position& pos)
+        {
+            return out << "Position{" << pos.x << ", " << pos.y << ", " << pos.dir << "}'";
+        }
+    };
+
+    enum Command
+    {
+        F,
+        B,
+        L,
+        R
+    };
+
+    class Rover
+    {
+    public:
+        Rover(int x, int y, Direction dir, std::optional<std::shared_ptr<World>> = std::nullopt);
+        Rover(Position pos, std::optional<std::shared_ptr<World>> = std::nullopt);
+        Position getPosition();
+        bool process_command(std::vector<Command> commands);
+    private:
+        Position pos;
+        std::optional<std::shared_ptr<World>> m_world;
+    };
+
+
+
+    class World
+    {
+        public:
+        World(std::vector<Position> obstacle)
+        {   
+            m_obstacle = obstacle;
+        }
+
+        bool isEmpty(Position objects){
+            for (const auto& objB : m_obstacle) {
+                    if (objects.x == objB.x && objects.y == objB.y) {
+                        return false; 
+                    }
+                }
+            return true; 
+       
+        }
+        private:
+        std::vector<Position> m_obstacle; 
+    };
+};
+
+
 
 #endif
