74b720b4a083085f55e60612cb593b6659b45a97
pietrik123 <piotrmierzwa042@gmail.com>
2023-11-23 15:00:25 +0000

    rover test

--- tdd-katas/catch2/mars-rover-catch/src/rover.hpp
+++ tdd-katas/catch2/mars-rover-catch/src/rover.hpp
@@ -13,6 +13,40 @@
 
 namespace TDD
 {
+    class ObstacleDetector
+    {
+    public:
+        virtual bool is_obstacle(int x, int y) const = 0;
+    };
+    
+    class PlanetInterface
+    {
+        virtual std::tuple<int, int, char> getEndPosition(
+            char currentDir, int currentX,
+            int currentY, char targetDir) = 0;
+    };
+
+    class Rover
+    {
+    public:
+        Rover(int x, int y, char dir, const ObstacleDetector* detector);
+        void run(const std::vector<char>& cmds);
+        void move(char cmd);
+        void turn(char cmd);
+        int getX() const;
+        int getY() const;
+        char getDir() const;
+        bool reportObstacle() const;
+    private:
+        int m_x;
+        int m_y;
+        char m_dir;
+        bool m_hitObstacle = false;
+        const ObstacleDetector* m_detector;
+        void checkObstacle(int x, int y);
+    };
+
+    
 }
 
 #endif
