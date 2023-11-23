2821ab651c16cd49c890a94879a365619dc6c500
AM350 <151860644+AM350@users.noreply.github.com>
2023-11-23 22:08:11 +0100

    Update recently_used_list.hpp

--- tdd-katas/gtest/recently-used-list/src/recently_used_list.hpp
+++ tdd-katas/gtest/recently-used-list/src/recently_used_list.hpp
@@ -9,8 +9,50 @@
 
 namespace TDD
 {
-	class RecentlyUsedList
-	{};
+    class MyException : public std::exception {
+    };
+
+    class StringList {
+    public:
+
+        StringList(uint8_t size = 0) :strList_(), size_(size) {};
+
+        std::deque<std::string> getList() {
+            return strList_;
+        }
+
+        void addToList(std::string elem)
+        {
+            if (elem.empty()) {
+                throw MyException();
+            }
+
+            std::deque<std::string>::iterator it = std::find(strList_.begin(), strList_.end(), elem);
+            if (it != strList_.end())
+            {
+                strList_.erase(it);
+            }
+
+            if (strList_.size() == size_)
+            {
+                strList_.erase(strList_.begin() + size_ - 1);
+            }
+            strList_.push_front(elem);
+        }
+
+        std::string getStrByIndex(uint8_t index)
+        {
+            return strList_[index];
+        }
+
+        uint8_t getMaxSize()
+        {
+            return size_;
+        }
+    private:
+        std::deque<std::string> strList_;
+        uint8_t size_;
+    };
 }
 
 #endif
