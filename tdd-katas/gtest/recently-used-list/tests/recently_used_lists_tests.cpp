5682d185584dbfb59c6d729b795bcd9c8ac1fcfe
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-23 22:25:27 +0100

    Formatting

--- tdd-katas/gtest/recently-used-list/tests/recently_used_lists_tests.cpp
+++ tdd-katas/gtest/recently-used-list/tests/recently_used_lists_tests.cpp
@@ -1,25 +1,28 @@
+#include "recently_used_list.hpp"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include <algorithm>
 #include <deque>
 #include <stdexcept>
-
-#include "recently_used_list.hpp"
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
 
 using namespace ::testing;
 
 class RecentlyUsedList
 {
-    private:
-        int max_capacity_;
-        std::deque<std::string> strList_{};
-    public:
-    RecentlyUsedList(int max_capacity = 5){
-        max_capacity_=max_capacity;
-        strList_={};
+private:
+    int max_capacity_;
+    std::deque<std::string> strList_{};
+
+public:
+    RecentlyUsedList(int max_capacity = 5)
+    {
+        max_capacity_ = max_capacity;
+        strList_ = {};
     }
 
-    int size() const {
+    int size() const
+    {
         return strList_.size();
     }
 
@@ -42,9 +45,9 @@ class RecentlyUsedList
         strList_.push_front(str);
 
         // Removing last element if max_capacity reached
-        if (strList_.size()>max_capacity_)
+        if (strList_.size() > max_capacity_)
         {
-            strList_.erase(strList_.end()-1);
+            strList_.erase(strList_.end() - 1);
         }
     }
 
@@ -61,12 +64,13 @@ class RecentlyUsedList
 
 class RecentlyUsedListTest : public ::testing::Test
 {
-protected:    
+protected:
     RecentlyUsedList list;
 };
 
 struct RUL_DefaultInitialized : RecentlyUsedListTest
-{};
+{
+};
 
 TEST_F(RUL_DefaultInitialized, ListIsEmpty)
 {
@@ -76,7 +80,7 @@ TEST_F(RUL_DefaultInitialized, ListIsEmpty)
 TEST_F(RUL_DefaultInitialized, WhenItemAddedListIsNotEmpty)
 {
     std::string item = "aaa";
-    list.add(item); 
+    list.add(item);
     EXPECT_EQ(list.size(), 1);
 }
 
@@ -99,10 +103,11 @@ TEST_F(RUL_WithItems, WhenAddingDuplicateSizeIsNotChanged)
 }
 
 struct RUL_Indexing : RUL_WithItems
-{};
+{
+};
 
 TEST_F(RUL_Indexing, ElementsCanBeLookedUpByTheirIndices)
-{   
+{
     EXPECT_EQ(list[0], "bbb");
 }
 
@@ -122,7 +127,7 @@ TEST_F(RUL_Indexing, ExceptionIsRaisedWhenGivenIndexGreaterThanListSize)
 TEST_F(RUL_DefaultInitialized, NullStringCannotBeAdded)
 {
     list.add("");
-    EXPECT_EQ(list.size(),0);
+    EXPECT_EQ(list.size(), 0);
 }
 
 struct RUL_params
@@ -134,26 +139,25 @@ struct RUL_params
 
 class RUL_WithItemsParamTests : public ::testing::TestWithParam<RUL_params>
 {
-    protected:    
+protected:
     RecentlyUsedList list{5};
 };
 
-RUL_params params[]={
+RUL_params params[] = {
     {"List with max capacity", {"aba", "aca", "ada", "aea", "afa", "aga"}, std::tuple{5, "aga", "aca"}},
     {"Adding 4 elements", {"ada", "aea", "afa", "aga"}, std::tuple{4, "aga", "ada"}},
     {"Adding 4 elements with one duplicate", {"aba", "aca", "aba", "aea"}, std::tuple{3, "aea", "aca"}},
-    {"Adding 4 elements with one empty string", {"aba", "aca", "aea", ""}, std::tuple{3, "aea", "aba"}}
-};
+    {"Adding 4 elements with one empty string", {"aba", "aca", "aea", ""}, std::tuple{3, "aea", "aba"}}};
 
 TEST_P(RUL_WithItemsParamTests, ListWithSetMaxCapacityParams)
 {
     const auto& param = GetParam();
-    for(const auto& item : param.items)
+    for (const auto& item : param.items)
         list.add(item);
-    
+
     auto [size, first, last] = param.expected;
 
-    EXPECT_EQ(list.size(), size);        // Assert
+    EXPECT_EQ(list.size(), size); // Assert
     EXPECT_EQ(list[0], first);
     EXPECT_EQ(list[size - 1], last);
 }
@@ -164,14 +168,14 @@ INSTANTIATE_TEST_SUITE_P(PackOfTests, RUL_WithItemsParamTests, ::testing::Values
 // {
 //     const auto capacity = 5;                 // Arrange
 //     RecentlyUsedList list{5};
-    
+
 //     list.add("aba");                         // Act
 //     list.add("aca");
 //     list.add("ada");
 //     list.add("aea");
 //     list.add("afa");
 //     list.add("aga");
-    
+
 //     EXPECT_EQ(list.size(), capacity);        // Assert
 //     EXPECT_EQ(list[0], "aga");
 //     EXPECT_EQ(list[capacity - 1], "aca");
