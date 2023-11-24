6ab94b62896b4dad978c0fd33d25d452f527c085
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-24 14:25:19 +0100

    DocEditApp - 3

--- document-editor/tests/application_tests.cpp
+++ document-editor/tests/application_tests.cpp
@@ -15,7 +15,12 @@ public:
 
     void run()
     {
-        console_.print("> Enter a command:");
+        do 
+        {
+            console_.print("> Enter a command:");
+            std::string cmd_text = console_.get_line();
+            if (cmd_text == "Exit") break;
+        } while (true);
     }
 private:
     Console& console_;
@@ -23,9 +28,30 @@ private:
 
 TEST(ApplicationTests, DisplaysPromptForCommand)
 {
-    MockConsole console;
+    NiceMock<MockConsole> console;
+    ON_CALL(console, get_line()).WillByDefault(Return("Exit"));
     EXPECT_CALL(console, print("> Enter a command:"));
 
     DocEditApp app(console);
     app.run();
+}
+
+TEST(ApplicationTests, GetLineFromConsole)
+{
+    NiceMock<MockConsole> console;
+    ON_CALL(console, get_line()).WillByDefault(Return("Exit"));
+    EXPECT_CALL(console, get_line()).Times(1);
+
+    DocEditApp app(console);
+    app.run();
+}
+
+TEST(Applicaton_MainLoop, ExitBreaksTheCommandLoop)
+{
+    MockConsole console;
+    EXPECT_CALL(console, get_line()).WillOnce(Return("Exit"));
+    EXPECT_CALL(console, get_line()).Times(3).WillRepeatedly(Return("cmd")).RetiresOnSaturation();
+
+    DocEditApp app(console);
+    app.run();   
 }
