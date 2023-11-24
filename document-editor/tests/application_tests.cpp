19a88d8c41273d3a2bf8a32f9693e552abfe4be6
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-24 14:45:02 +0100

    DocEditApp - 4

--- document-editor/tests/application_tests.cpp
+++ document-editor/tests/application_tests.cpp
@@ -26,32 +26,60 @@ private:
     Console& console_;
 };
 
-TEST(ApplicationTests, DisplaysPromptForCommand)
+struct ApplicationMainLoop : ::testing::Test
 {
-    NiceMock<MockConsole> console;
-    ON_CALL(console, get_line()).WillByDefault(Return("Exit"));
+    MockConsole console;
+    DocEditApp app{console};
+
+    void SetUp() override
+    {
+        EXPECT_CALL(console, print(_)).WillRepeatedly(Return());
+        EXPECT_CALL(console, get_line()).WillOnce(Return("Exit"s)).RetiresOnSaturation();
+    }
+};
+
+TEST_F(ApplicationMainLoop, DisplaysPromptForCommand)
+{
     EXPECT_CALL(console, print("> Enter a command:"));
 
     DocEditApp app(console);
     app.run();
 }
 
-TEST(ApplicationTests, GetLineFromConsole)
+TEST_F(ApplicationMainLoop, GetLineFromConsole)
 {
-    NiceMock<MockConsole> console;
-    ON_CALL(console, get_line()).WillByDefault(Return("Exit"));
-    EXPECT_CALL(console, get_line()).Times(1);
+    EXPECT_CALL(console, get_line()).Times(1).RetiresOnSaturation();
 
     DocEditApp app(console);
     app.run();
 }
 
-TEST(Applicaton_MainLoop, ExitBreaksTheCommandLoop)
+TEST_F(ApplicationMainLoop, ExitBreaksTheCommandLoop)
 {
-    MockConsole console;
-    EXPECT_CALL(console, get_line()).WillOnce(Return("Exit"));
     EXPECT_CALL(console, get_line()).Times(3).WillRepeatedly(Return("cmd")).RetiresOnSaturation();
 
     DocEditApp app(console);
     app.run();   
+}
+
+//////////////////////////////////////////////
+TEST(GMockTests, RetiresOnSatureation)
+{
+    MockConsole console;
+
+    //EXPECT_CALL(console, get_line()).Times(1).WillRepeatedly(Return("Exit"));
+    EXPECT_CALL(console, print(_)).WillRepeatedly(Return());
+    // EXPECT_CALL(console, get_line()).Times(3).WillRepeatedly(Return("Line"));
+    EXPECT_CALL(console, get_line()).WillOnce(Return("Line1"))
+        .WillOnce(Return("Line2"))
+        .WillOnce(Return("Line3"))
+        .WillRepeatedly(Return("Exit"));
+
+    ASSERT_EQ(console.get_line(), "Line1");
+    ASSERT_EQ(console.get_line(), "Line2");
+    ASSERT_EQ(console.get_line(), "Line3");
+    ASSERT_EQ(console.get_line(), "Exit");
+    ASSERT_EQ(console.get_line(), "Exit");
+    ASSERT_EQ(console.get_line(), "Exit");
+    ASSERT_EQ(console.get_line(), "Exit");
 }
